d68c0c0fd33c8387c815c2b3295432565955d60d
sandeeeerr <sandervries@me.com>
2023-10-15 15:20:16 +0200

    asd

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="px-4 mb-10 text-center text-gray-500">
+    <footer className="px-4 mt-10 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs">
         &copy; 2023 Sander. All rights reserved.
       </small>
