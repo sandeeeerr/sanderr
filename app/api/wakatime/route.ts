5759c8b03496350896085f138180fe35b0e3d016
sandeeeerr <sandervries@me.com>
2023-10-31 13:17:34 +0100

    Update route.ts

--- app/api/wakatime/route.ts
+++ app/api/wakatime/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-// import { env } from '@/env'
+import { env } from '@/env'
 
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
@@ -10,7 +10,7 @@ export const GET = async () => {
     'https://wakatime.com/api/v1/users/current/all_time_since_today',
     {
       headers: {
-        Authorization: `Basic ${Buffer.from("waka_fc868abe-2ed2-4b75-8f0c-7804b5b79b10").toString(
+        Authorization: `Basic ${Buffer.from(env.WAKATIME_API_KEY).toString(
           'base64'
         )}`
       }
